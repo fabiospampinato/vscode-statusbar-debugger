f5528d817fa070f98dde0c5569e97bea54f5cf0b
Fabio Spampinato <spampinabio@gmail.com>
2017-07-16 16:56:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# VSC StatusBar Debugger
+
+<p align="center">
+	<img src="https://raw.githubusercontent.com/fabiospampinato/vscode-statusbar-debugger/master/resources/logo-128x128.png" alt="Logo">
+</p>
+
+Add a debugger to the statusbar, less intrusive than the default floating one.
+
+## Install
+
+Run the following in the command palette:
+
+```shell
+ext install vscode-statusbar-debugger
+```
+
+## Usage
+
+It adds 3 new commands to the command palette:
+
+```js
+StatusBar Debugger: Toggle // Toggle debugging
+StatusBar Debugger: Start // Start debugging
+StatusBar Debugger: Stop // Stop debugging
+```
+
+It also adds a bug-like icon to the statusbar, you can use it to toggle debugging. Once a debugging session has started the usual actions will appear next to it.
+
+## Demo
+
+![Demo](resources/demo.gif)
+
+## Hits:
+
+- **Disable the default debugger**: set `"debug.hideActionBar": true` in your settings to disable the default, intrusive debugger.
+- **Remap start/stop shortcuts**: due to some limitations to the API this extension cannot properly detect if you start/stop debugging with other means than those provided by this extension, therefor is advisable to remap your start/stop shortcuts to it (they will work the same, but this extension will be able to detect when they are triggered), add this to your `keybindings.json`:
+```json
+      { "key": "f5",       "command": "statusbarDebugger.start",
+                             "when": "!inDebugMode" },
+      { "key": "shift+f5", "command": "statusbarDebugger.stop",
+                             "when": "inDebugMode"}
+```
+
+## License
+
+MIT © Fabio Spampinato
---
+++ src/statusbar.ts
@@ -0,0 +1,75 @@
+
+/* IMPORT */
+
+import * as _ from 'lodash';
+import * as vscode from 'vscode';
+
+/* STATUSBAR */
+
+class Statusbar {
+
+  _isActive; bug; actions;
+
+  constructor () {
+
+    /* BUG */
+
+    const bugOptions = { text: '$(bug)', tooltip: 'Start debugging', command: 'statusbarDebugger.toggle' };
+
+    this.bug = this.makeItem ( bugOptions );
+    this.bug.show ();
+
+    /* ACTIONS */
+
+    const actionsOptions = [
+      // { text: '$(quote)', tooltip: 'Pause', command: 'workbench.action.debug.pause' },
+      { text: '$(triangle-right)', tooltip: 'Continue', command: 'workbench.action.debug.continue' },
+      { text: '$(arrow-right)',tooltip: 'Step over', command: 'workbench.action.debug.stepOver' },
+      { text: '$(arrow-down)', tooltip: 'Step into', command: 'workbench.action.debug.stepInto' },
+      { text: '$(arrow-up)', tooltip: 'Step out', command: 'workbench.action.debug.stepOut' },
+      { text: '$(mail-reply)', tooltip: 'Restart', command: 'workbench.action.debug.restart' },
+      { text: '$(primitive-square)', tooltip: 'Stop', command: 'statusbarDebugger.stop' }
+    ];
+
+    this.actions = actionsOptions.map ( this.makeItem );
+
+  }
+
+  makeItem ( options ) {
+
+    const item = vscode.window.createStatusBarItem ( vscode.StatusBarAlignment.Left, -Infinity );
+
+    _.extend ( item, options );
+
+    return item;
+
+  }
+
+  isActive () {
+
+    return !!this._isActive;
+
+  }
+
+  toggle ( force?: boolean ) {
+
+    const active = _.isBoolean ( force ) ? force : !this._isActive;
+
+    /* BUG */
+
+    this.bug.color = active ? '#B89500' : undefined;
+    this.bug.tooltip = active ? 'Stop debugging' : 'Start debugging';
+
+    /* ACTIONS */
+
+    this.actions.forEach ( action => action[active ? 'show' : 'hide']() );
+
+  }
+
+}
+
+/* EXPORT */
+
+const statusbar = new Statusbar ();
+
+export default statusbar;
